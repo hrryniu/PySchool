341d7e06bb371a4a48b12b1abfaf985223825ae7
hrryniu <62951083+hrryniu@users.noreply.github.com>
2025-01-08 21:14:58 +0100

    oop

--- learn2/44_oop_classes.py
+++ learn2/44_oop_classes.py
@@ -1,23 +1,38 @@
 class Student:
-    def __init__(self,name,house):
+    def __init__(self,name,house, patronus):
         if not name:
             raise ValueError("Missing name")
         if house not in ["Gryffindor", "Huffelpuff", "Revenclaw", "Slytherin"]:
             raise ValueError("Invalid house")
         self.name= name
         self.house= house
+        self.patronus= patronus
 
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+    def charm(self):
+        match self.patronus:
+            case "Stag":
+                return  "🪭"
+            case "Otter":
+                return " 🌈"
+            case "Jack Russel terrier":
+                return "🛞"
+            case _:
+                return "🥸"
 
 def main():
     student= get_student()
-    print(f"{student.name} from {student.house}")
+    print("Expecto patronum!")
+    print(student.charm())
 
 
 def get_student():
     name= input("Name: ")
     house= input("House: ")
-    student= Student(name,house)
-    return student
+    patronus= input("Patronus: ")
+    return Student(name,house,patronus)
 
 if __name__ == "__main__":
     main()

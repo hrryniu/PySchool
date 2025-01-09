18eb221ce7151ece8c6b198d07103f7e4a34f007
hrryniu <62951083+hrryniu@users.noreply.github.com>
2025-01-09 11:54:41 +0100

    revisions of lessons

--- learn2/49_argparse.py
+++ learn2/49_argparse.py
@@ -1,8 +1,8 @@
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-n")
+parser = argparse.ArgumentParser(description="Meow like a cat")
+parser.add_argument("-n", default=1, help="number of times to meow")
 args= parser.parse_args()
-
+#w linijce 3 wartość default jest potrezbna bo inaczej bez podania arguentu n- program scrashuje
 for _ in range(int(args.n)):
     print("meow")
---
+++ learn2/50_unpack.py
@@ -0,0 +1,19 @@
+def total(galleons, sickles, knuts):
+    return (galleons*17+sickles)*29+knuts
+
+
+# prostacki sposób
+#print(total(100,50,25), "Knuts")
+
+#---------
+#sopsob podatny na błędy
+# coins=[100,50,25]
+# print(total(coins[0],coins[1],coins[2]), "Knuts")
+
+#-----metoda unpadking, rozkowuje poszczególne elementy listy
+coins=[100,50,25]
+print(total(*coins), "Knuts")
+
+#można też rozpakować słownik/distionary/ ale to tego potrzeba wpisać nie jedną ale dwie gwiazdki
+
+#jeśli w liście pojawi się jakiś nowy argument to rozpakowanie się nie uda bo funkcja total potrzebuje tylko trzech argumentów

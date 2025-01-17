df2021534388776b48843a988c144f22303a9753
hrryniu <62951083+hrryniu@users.noreply.github.com>
2025-01-17 20:56:14 +0100

    turtle race progress

--- learn2/56_turtle_race.py
+++ learn2/56_turtle_race.py
@@ -1,5 +1,44 @@
+import turtle #performs 2d graphic operations
+import time
+
+#definiowanie rozmiaru okna
+WIDTH,HEIGHT = 500, 500 # capital letters bo to wartości constant, nie zmienią się przez cały czas trwania programu
+COLORS = ['red', 'blue', 'orange', 'yellow', 'green' , 'brown' , 'cyan']
+
+
 def get_number_of_racers():
     racers = 0
     while True:
         racers=input("Enter the number of racers(2-10): ")
         if racers.isdigit():
+            racers= int(racers)
+            #break
+        else:
+            print("Input is not numeric. Try Again!")
+            continue
+        if 2<= racers <= 10:
+            return racers
+        else:
+            print("Input in not from 2-10 range")
+            # continue  #nie trzeba dawać tu continue bo po tym statemencie nic nie ma
+
+
+def init_turtle():
+    screen = turtle.Screen()
+    screen.setup(WIDTH, HEIGHT)
+    screen.title('Turtle Racing!')
+
+
+racers= get_number_of_racers()
+init_turtle()
+
+racer = turtle.Turtle()
+racer.speed(2)
+racer.shape('turtle')
+reacer.color('green')
+racer.forward(100) #ilość pikseli o które obiekt ma się przesunać
+racer.left(90) #przy skręcie w lewo i prawo podaje się kąt jako argument funkcji
+racer.forward(100)
+racer.left(90)
+racer.backward(100)
+

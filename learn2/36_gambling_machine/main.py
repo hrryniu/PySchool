054f7c26e83ee0bdb1b8a1343f06229731689c8b
hrryniu <62951083+hrryniu@users.noreply.github.com>
2025-01-17 14:21:44 +0100

    another lessons

--- learn2/36_gambling_machine/main.py
+++ learn2/36_gambling_machine/main.py
@@ -1,75 +1,79 @@
-import random   #bedziemy losowo generowac wynik "slot machine"
-#from traceback import print_tb
+import random
 
-MAX_LINES=3  #wartości stałe(constant wpisujemy wielkimi literami)
+MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
 
-ROWS=3  #definiujemy rozmiar maszyny losującej
-COLS=3
+ROWS = 3
+COLS = 3
 
-symbol_count={     #generujemy ilość znaków w
-                    # każdej linii maszyny
-    "A":2,             #to jest SŁOWNIK
-    "B":4,
-    "C":6,
-    "D":8
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
 }
 
-symbol_value={
-    "A":5,             #to jest SŁOWNIK
-    "B":4,
-    "C":2,
-    "D":2
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
 }
 
-def check_winnings(columns, lines, bet,values):
-    winnings= 0
-    winning_lines=[]
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = []
     for line in range(lines):
-        symbol= columns[0][line]
+        symbol = columns[0][line]
         for column in columns:
-            symbol_to_check= columns[line]
+            symbol_to_check = column[line]
             if symbol != symbol_to_check:
                 break
         else:
             winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
 
-    return winnings
+    return winnings, winning_lines
 
-def get_slot_machine_spin(rows,cols,symbols): #ta funkcja będzie losować symbole do 3 rzędów w 3 kolumnach
-    all_symbols = []  #tworzymy listę w której są wszystkie symbole z której będziemy 3 losowe elementy
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
     for symbol, symbol_count in symbols.items():
-        for _ in range(symbol_count): #underscore to anonimowa zmienna, dzieki temu nie marnujemy znaków na jakieś inne ważniejsze zmienne
+        for _ in range(symbol_count):
             all_symbols.append(symbol)
 
-    columns =  []
-    for _ in range(cols): #dla każdej kolumny(których mamy zdeklarowanych 3) muszę petlą wygenerować tyle symboli ile jest rzędów
-        column=[]
-        current_symbols=all_symbols[:] #[:] kopiuje wartości z all_symbols do current_symbols- chodzi o to że dla każdej
-        for _ in range(rows):           #kolummy losujemy z nowej instacji listy all symbols. all symbols to lista
-            value= random.choice(current_symbols) # wszystkich znaków jakie mogą się pojawić w kolumnie, jeśli wykorzystam w
-            current_symbols.remove(value)    #pierwszej kolumnie np. 2 znaki A i 1 B to chcę mieć możliwość wykorzystania tych samych znaków w kolejnej kolumnie. dlatego kopiuję
-            columns.append(value) #tutaj nastepuje usuniecie wykorzystanego symbolu
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
 
         columns.append(column)
 
     return columns
 
+
 def print_slot_machine(columns):
     for row in range(len(columns[0])):
         for i, column in enumerate(columns):
-            if i != len(columns) -1:
-                print(column[row], end=" | ") #generuję separator na konczy kazdej kolumny
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
             else:
                 print(column[row], end="")
+
         print()
 
+
 def deposit():
-    while True: #petla while działa dopóki z niej nie wyjdziemy funkcją break
-        amount = input("What would You like to deposit $ ")
-        if amount.isdigit(): #walidacja inputu: czy to liczba?
-            amount= int(amount)
+    while True:
+        amount = input("What would you like to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
             if amount > 0:
                 break
             else:
@@ -79,11 +83,13 @@ def deposit():
 
     return amount
 
+
 def get_number_of_lines():
     while True:
-        lines = input("Enter the number of lines to bet on(1-" + str(MAX_LINES)+ ")? ")
+        lines = input(
+            "Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
         if lines.isdigit():
-            lines= int(lines)
+            lines = int(lines)
             if 1 <= lines <= MAX_LINES:
                 break
             else:
@@ -93,36 +99,55 @@ def get_number_of_lines():
 
     return lines
 
+
 def get_bet():
     while True:
-        amount = input("What would You like to bet on each line? $ ")
+        amount = input("What would you like to bet on each line? $")
         if amount.isdigit():
-            amount= int(amount)
+            amount = int(amount)
             if MIN_BET <= amount <= MAX_BET:
                 break
             else:
-                print(f"Amount must be between {MIN_BET} - ${MAX_BET}.")
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")
         else:
             print("Please enter a number.")
+
     return amount
 
-def main():
-    balance= deposit()
-    lines= get_number_of_lines()
-    while True: #ta pętla sprawdza czy mamy wystarczającą ilość pieniędzy
-        bet= get_bet()
-        total_bet=bet*lines
+
+def spin(balance):
+    lines = get_number_of_lines()
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
 
         if total_bet > balance:
-            print(f"You do not have enough to bet that amount, your current balance is: $ {balance}")
+            print(
+                f"You do not have enough to bet that amount, your current balance is: ${balance}")
         else:
             break
 
-    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+    print(
+        f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
 
-    slots= get_slot_machine_spin(ROWS,COLS,symbol_count)
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
     print_slot_machine(slots)
-    winnings= check_winnings(slots,lines,bet,symbol_value)
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
     print(f"You won ${winnings}.")
+    print(f"You won on lines:", *winning_lines)
+    return winnings - total_bet
+
+
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance is ${balance}")
+        answer = input("Press enter to play (q to quit).")
+        if answer == "q":
+            break
+        balance += spin(balance)
+
+    print(f"You left with ${balance}")
+
 
 main()
---
+++ learn2/56_turtle_race.py
@@ -0,0 +1,5 @@
+def get_number_of_racers():
+    racers = 0
+    while True:
+        racers=input("Enter the number of racers(2-10): ")
+        if racers.isdigit():

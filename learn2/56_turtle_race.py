ec9cf6d9c65cb59f39b5ffb82132479fa2013abe
hrryniu <62951083+hrryniu@users.noreply.github.com>
2025-01-18 12:39:54 +0100

    another lesson

--- learn2/56_turtle_race.py
+++ learn2/56_turtle_race.py
@@ -1,26 +1,53 @@
-import turtle #performs 2d graphic operations
+import turtle
 import time
+import random
 
-#definiowanie rozmiaru okna
-WIDTH,HEIGHT = 500, 500 # capital letters bo to wartości constant, nie zmienią się przez cały czas trwania programu
-COLORS = ['red', 'blue', 'orange', 'yellow', 'green' , 'brown' , 'cyan']
-
+WIDTH, HEIGHT = 700, 600 # capital letters bo to wartości constant, nie zmienią się przez cały czas trwania programu
+COLORS = ['red', 'blue', 'orange', 'yellow', 'green' , 'brown' , 'cyan', 'black', 'purple']
 
 def get_number_of_racers():
     racers = 0
     while True:
-        racers=input("Enter the number of racers(2-10): ")
+        racers = input("Enter the number of racers(2-10): ")
         if racers.isdigit():
-            racers= int(racers)
-            #break
+            racers = int(racers)
         else:
             print("Input is not numeric. Try Again!")
             continue
-        if 2<= racers <= 10:
+
+        if 2 <= racers <= 10:
             return racers
         else:
             print("Input in not from 2-10 range")
             # continue  #nie trzeba dawać tu continue bo po tym statemencie nic nie ma
+
+def race(colors):
+    turtles = create_turtles(colors)
+
+    while True:
+        for racer in turtles:
+            distance = random.randrange(1,20)
+            racer.forward(distance)
+
+            x,y = racer.pos()
+            if y >= HEIGHT //2 - 10:
+                return colors[turtles.index(racer)]
+
+
+def create_turtles(colors):
+    turtles = []
+    spacingx= WIDTH // (len(colors) +1)
+    for i, color in enumerate(colors):
+        racer = turtle.Turtle()
+        racer.color(color)
+        racer.shape('turtle')
+        racer.left(90)
+        racer.penup()
+        racer.setpos(-WIDTH//2 +(i+1)* spacingx, -HEIGHT//2+20)
+        racer.pendown()
+        turtles.append(racer)
+
+    return turtles
 
 
 def init_turtle():
@@ -29,16 +56,12 @@ def init_turtle():
     screen.title('Turtle Racing!')
 
 
-racers= get_number_of_racers()
+racers = get_number_of_racers()
 init_turtle()
 
-racer = turtle.Turtle()
-racer.speed(2)
-racer.shape('turtle')
-reacer.color('green')
-racer.forward(100) #ilość pikseli o które obiekt ma się przesunać
-racer.left(90) #przy skręcie w lewo i prawo podaje się kąt jako argument funkcji
-racer.forward(100)
-racer.left(90)
-racer.backward(100)
+random.shuffle(COLORS)
+colors = COLORS[:racers]
+
+winner = race(colors)
+print(winner)
 

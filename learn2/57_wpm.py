d5f96932ae453588b10fa2bcac201243e02537a9
hrryniu <62951083+hrryniu@users.noreply.github.com>
2025-01-21 13:35:14 +0100

    reorganisation

--- learn2/57_wpm.py
+++ learn2/57_wpm.py
@@ -1,19 +1,52 @@
-import curses #pozwala modyfikować terminal
+import curses
 from curses import wrapper
+import time
+import random
+
 
 def start_screen(stdscr):
     stdscr.clear()
     stdscr.addstr("Welcome to the Speed Typing Test!")
     stdscr.addstr("\nPress any key to begin!")
     stdscr.refresh()
-    stdscr.gotkey()
+    stdscr.getkey()
+
+def display_text(stdscr, target, current, wpm=0):
+
+    stdscr.addstr(target_text)
+
+    for i, char in enumerate(current_text):
+        correct_char = target[i]
+        color = curses.color_pair(1)
+        if char != correct_char:
+            color = curses.color_pair(2)
+        stdscr.addstr(char, curses.color_pair(1))
 
 def wpm_test(stdscr):
     target_text = "Hello world this is some test text for this app!"
     current_text =[]
-    stdscr.clear()
-    stdscr.addstr(target_text)
+
+    while True:
+        key = stdscr.clear()
+        current_text.append(key)
+
+        stdscr.clear()
+
+
     stdscr.refresh()
+
+    while True:
+        key = stdscr.getkey()
+        if ord(key) == 27:
+            break
+        if key in ("KEY_BACKSPACE", '\b', "\x7f"):
+            if len(current_text) > 0:
+                current_text.pop()
+        elif len(current_text) < len(target_text):
+            current_text.append(key)
+
+        current_text.append(key)
+
 
 
 def main(stdscr): #standart screen
